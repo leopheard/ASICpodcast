5a333b6f3162835c739b6833c05c4c64d9fb4a54
leopheard <leopheard.misc@gmail.com>
2019-09-06 21:49:26 -0400

    upload

---
+++ README.md
@@ -0,0 +1,10 @@
+<a href="https://asic.gov.au/about-asic/news-centre/videos-and-podcasts/the-asic-podcast/">ASIC</a> podcast.<br>
+
+<a href="kodi.tv">KODI<a> / XBMC / Firestick addon for the <a href="https://asic.gov.au/about-asic/news-centre/videos-and-podcasts/the-asic-podcast/">ASIC</a> podcast (Australian Securities and Investments Commission).<br>
+
+ASIC podcasts about what we do and the stories we encounter along the way<br>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<img src="https://pbcdn1.podbean.com/imglogo/image-logo/859234/ASIC_Podcast_large_size.png">
+<br><a href="http://www.kodi.tv"><img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
---
+++ resources/lib/mainaddon.py
@@ -0,0 +1,41 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url1 = "https://asic.podbean.com/feed.xml"
+
+def get_soup1(url1):
+    page = requests.get(url1)
+    soup1 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup1))
+    return soup1
+get_soup1("https://asic.podbean.com/feed.xml")
+
+def get_playable_podcast1(soup1):
+    subjects = []
+    for content in soup1.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': "https://pbcdn1.podbean.com/imglogo/image-logo/859234/ASIC_Podcast_large_size.png",
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast1(playable_podcast1):
+    items = []
+    for podcast in playable_podcast1:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
