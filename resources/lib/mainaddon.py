1820c5945b29e6a11ddad39322094b1adf273688
leopheard <leopheard.misc@gmail.com>
2019-09-06 23:31:04 -0400

    upload

--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -13,7 +13,7 @@ get_soup1("https://asic.podbean.com/feed.xml")
 
 def get_playable_podcast1(soup1):
     subjects = []
-    for content in soup1.find_all('item'):
+    for content in soup1.find_all('item' limit=9):
         try:        
             link = content.find('enclosure')
             link = link.get('url')
